a77e6be24bef5ce379729a3d95f42bc44fe39a2e
Disolm <63400086+Disolm@users.noreply.github.com>
2022-10-22 22:28:07 +0700

    Added telegram api

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,17 +1,17 @@
 <template>
-  <section>
-    <div class="error">
+  <div class="error">
+    <div class="error__title">
       <h1>Error 404</h1>
-      <h2>Такой страницы не существует или ещё в разработке</h2>
+      <h2>Такой страницы не существует или она находится ещё в разработке</h2>
       <h3>Это всего лишь ошибка на сайте, ресторан по прежнему работаем</h3>
-      <nuxt-link
-        class="error__link"
-        to="/"
-      >
-        Вернусться на главную страницу
-      </nuxt-link>
     </div>
-  </section>
+    <nuxt-link
+      class="error__link"
+      to="/"
+    >
+      Вернусться на главную страницу
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
@@ -22,18 +22,25 @@ export default {
 
 <style lang="scss">
   .error {
-    height: 400px;
+    height: 100vh;
     width: 100%;
     text-align: center;
-    padding-top: 100px;
+    //padding-top: 100px;
     background-color: #FFF2E7;
+    margin-top: 0;
+    &__title {
+      @include padding-top-main;
+      margin: 42px;
+    }
     &__link {
+      font-size: 20px;
       border: 2px solid rgba(225, 255, 255, 0);
     }
     &__link:hover {
-      border-bottom: 2px solid rgba(0, 0, 0, 1);
+      //border-bottom: 2px solid rgba(0, 0, 0, 1);
       cursor: pointer;
       color: $brown;
+      text-decoration: underline;
     }
   }
   h1 {
---
+++ pages/cart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="cart">
+    <div class="cart__form">
+      <input v-model="message">
+      <button @click="sendMessage">Отправить сообщение</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import telegram_data from '/static/json/telegram-api.json'
+import * as axios from 'axios'
+
+export default {
+  name: "cart",
+  scrollToTop: true,
+  data () {
+    return {
+      telegram: telegram_data,
+      message: ''
+    }
+  },
+
+  methods: {
+    sendMessage() {
+      axios.get(`https://api.telegram.org/bot${telegram_data.token}/sendMessage?chat_id=@${telegram_data['id-group']}&text=${this.message}`)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.cart {
+  margin: 0;
+  height: 100%;
+  background-color: $white;
+  &__form {
+    padding-top: 42px;
+    width: 160px;
+  }
+}
+</style>
